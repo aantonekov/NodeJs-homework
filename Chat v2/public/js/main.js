345a9e1c6afac9cfe0fdd49ce4a6cd12d05f1284
aantonenkov <78919513+aantonekov@users.noreply.github.com>
2021-08-05 00:16:58 +0300

    edition1

---
+++ Chat v2/public/js/main.js
@@ -0,0 +1,35 @@
+const socket = io();
+
+const form = document.querySelector('.form');
+const messages = document.querySelector('.messages');
+const input = document.querySelector('.input');
+const nameBlock = document.querySelector('.name');
+
+const userName = prompt('Ваше имя:')
+nameBlock.innerHTML = `${userName}`
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    
+    // input.addEventListener('keypress', (ev) => {
+    //     ev.preventDefault();
+    //     console.log('typing...');
+    // })
+
+    if ( input.value ) {
+        socket.emit('chat message', { 
+            message: input.value, 
+            name: userName 
+        });
+        input.value = ``;
+    }
+})
+
+
+
+socket.on('chat message', (data) => {
+    const item = document.createElement('li')
+    item.innerHTML = `<span>${data.name}</span>: ${data.message}`
+    messages.appendChild(item)
+    window.scrollTo(0,document.body.scrollHeight)
+})
